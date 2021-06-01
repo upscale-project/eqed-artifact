533cd12b2d1609da51bb96d48b76bcb874206bfe
Eshan Singh <esingh@rsg21.stanford.edu>
2021-06-01 16:33:09 -0700

    Updates to CCX run scipt and patch files

--- examples/run/jasper_ost2.tcl
+++ examples/run/jasper_ost2.tcl
@@ -1,7 +1,7 @@
-
+clear -all
 analyze -sv09 -f ost2.flist 
 elaborate -disable_auto_bbox -top eqed_ost2
 clock clk
-reset -expression eqed_rst
+reset -init_state reset.init
 set_engine_mode {B Bm}
-autoprove
+autoprove -effort high
